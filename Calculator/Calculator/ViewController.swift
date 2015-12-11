d8754cea87940627ed080838d9dd8ca2fdd2d1a9
mattcmf <mattcmf@gmail.com>
2015-12-11 06:38:55 +0000

    Chopping out none tutorial code for master

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -11,29 +11,15 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var display: UILabel!
+    
     var userIsInTheMiddleOfTyping: Bool = false
-    var containsDecimalPlace: Bool = false
-    var Action: String!
-    var initialValue: Int = 0
     
     @IBAction func appendDigit(sender: UIButton) {
         let digit = sender.currentTitle!
         if userIsInTheMiddleOfTyping {
-            if (digit == ".")
-            {
-                if (containsDecimalPlace == false)
-                {
-                    display.text = display.text! + digit
-                }
-            }
-            else
-            {
-                display.text = display.text! + digit
-            }
-            
+            display.text = display.text! + digit
         }
-        else
-        {
+        else {
            display.text = digit
            userIsInTheMiddleOfTyping = true
         }
